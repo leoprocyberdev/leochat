a9ab1b0faae82346427edb2cbaabfdca12a115a9
leoprocyberdev <leopoldtwinomujuni@gmail.com>
2026-07-20 17:53:46 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'leochat-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/dashboard.html',
+  '/chat.html',
+  '/js/cloudinary.js',
+  '/manifest.json'
+];
+
+// Install Event
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Fetch Event
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
